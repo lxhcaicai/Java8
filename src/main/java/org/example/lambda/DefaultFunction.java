c935f5b17155e46df79a95e3e6f5487eacf5f516
lxhcaicai <2778763221@qq.com>
2023-03-01 21:49:54 +0800

    接口静态方法的使用

--- src/main/java/org/example/lambda/DefaultFunction.java
+++ src/main/java/org/example/lambda/DefaultFunction.java
@@ -2,29 +2,20 @@ package org.example.lambda;
 
 public class DefaultFunction {
     public static void main(String[] args) {
-        BB bb = new BB();
-        bb.show();
-
-        CC cc = new CC();
-        cc.show();
+        AA.show();
     }
 }
 
 interface AA {
-    default void show() {
-        System.out.println("我是AA接口的默认方法");
+    //● 直接使用接口名调用即可：接口名.静态方法名();
+    //● 常用接口中的静态方法不能被继承，也不能被重写。
+
+    static void show() {
+        System.out.println("我是AA接口的静态方法");
     }
 }
 
 //默认方法的使用方式一：实现类可以直接使用
 class BB implements AA {
 
-}
-
-//默认方法的使用方式二：实现类可以重写默认方法
-class CC implements AA {
-    @Override
-    public void show() {
-        System.out.println("我是CC类重现默认方法");
-    }
 }
