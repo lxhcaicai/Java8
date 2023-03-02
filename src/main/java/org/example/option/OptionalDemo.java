1912a155611a91cd1b0f25993b8730056df5fa09
lxhcaicai <2778763221@qq.com>
2023-03-02 20:10:04 +0800

    Optional 类的基本使用： 判断Optional类中是否有值  获取Optional类中的值

--- src/main/java/org/example/option/OptionalDemo.java
+++ src/main/java/org/example/option/OptionalDemo.java
@@ -36,4 +36,31 @@ public class OptionalDemo {
         optional = Optional.ofNullable("你好啊！");
         System.out.println("optional = " + optional);
     }
+
+    /**
+     * 判断Optional类中是否有值
+     */
+    @Test
+    public void test() {
+        Optional<Object> empty = Optional.empty();
+        if(empty.isPresent()) {
+            System.out.println("Optional类中有值");
+        } else {
+            System.out.println("Optional类中没有值");
+        }
+    }
+
+    /**
+     * 获取Optional类中的值
+     */
+    @Test
+    public void test2() {
+        Optional<String> optional = Optional.ofNullable("你好，世界");
+        if(optional.isPresent()) {
+            //get()方法，可以用来获取Optional类中的值，如果有值就返回具体值，否则就报错。
+            //一般get()方法配置isPresent()方法使用
+            String str = optional.get();
+            System.out.println("str = " + str);
+        }
+    }
 }
