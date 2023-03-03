7b85925c27231834b15a7222231099b3197ccde1
lxhcaicai <2778763221@qq.com>
2023-03-03 22:46:32 +0800

    构造 String 对象: 构造方法

--- src/main/java/org/example/string/demo1/DoTest.java
+++ src/main/java/org/example/string/demo1/DoTest.java
@@ -2,6 +2,8 @@ package org.example.string.demo1;
 
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+
 public class DoTest {
 
     @Test
@@ -20,5 +22,37 @@ public class DoTest {
         String str = "abc";
         String str2 = new String("abc");
         System.out.println(str == str2);
+    }
+
+    /**
+     *  构造 String 对象
+     */
+    @Test
+    public void test3() throws UnsupportedEncodingException {
+        // 字符串常量对象
+        String str = "hello";
+        System.out.println("str = " + str);
+
+        // 无参构造
+        String str1 = "";
+        System.out.println("str1 = " + str1);
+
+        // 创建"hello"字符串常量的副本
+        String str2 = new String("Hello");
+        System.out.println("str2 = " + str2);
+
+        // 通过字符数组创建
+        char[] chs = {'a', 'b', 'c', 'd'};
+        String str3 = new String(chs);
+        String str4 = new String(chs, 0, 3);
+        System.out.println("str3 = " + str3);
+        System.out.println("str4 = " + str4);
+
+        // 通过字节数组构造
+        byte[] bytes = {97, 98, 99};
+        String str5 = new String(bytes);
+        String str6 = new String(bytes, "utf-8");
+        System.out.println("str5 = " + str5);
+        System.out.println("str6 = " + str6);
     }
 }
