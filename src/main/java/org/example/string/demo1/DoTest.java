64fe343dd163cc53a2cfc2d9165b1f5fc703a704
lxhcaicai <2778763221@qq.com>
2023-03-03 23:05:52 +0800

    == 比较的是对象的地址，只有两个字符串变量都指向字符串的常量对象时，才会返回 true

--- src/main/java/org/example/string/demo1/DoTest.java
+++ src/main/java/org/example/string/demo1/DoTest.java
@@ -138,4 +138,16 @@ public class DoTest {
         System.out.println(s3 == s5); // true
         System.out.println(s1 + s2 == s5); // false
     }
+
+    @Test
+    public void test10(){
+        String s1 = "hello";
+        String s2 = "hello";
+        System.out.println(s1 == s2);
+
+        String s3 = new String("hello");
+        String s4 = new String("hello");
+        System.out.println(s3 == s4);
+        System.out.println(s1 == s4);
+    }
 }
