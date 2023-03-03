c3796286d6979ec1463a0521e7c7be799502366a
lxhcaicai <2778763221@qq.com>
2023-03-03 23:06:49 +0800

    equals() 方法比较的是对象的内容，因为 String 类重写了 equals() 方法，并且 equals() 方法区分大小写

--- src/main/java/org/example/string/demo1/DoTest.java
+++ src/main/java/org/example/string/demo1/DoTest.java
@@ -150,4 +150,17 @@ public class DoTest {
         System.out.println(s3 == s4);
         System.out.println(s1 == s4);
     }
+
+    @Test
+    public void test11() {
+        String s1 = "hello";
+        String s2 = "hello";
+
+        System.out.println(s1.equals(s2)); // true
+
+        String s3 = new String("hello");
+        String s4 = new String("hello");
+        System.out.println(s3.equals(s4)); // true
+        System.out.println(s1.equals(s4)); // true
+    }
 }
