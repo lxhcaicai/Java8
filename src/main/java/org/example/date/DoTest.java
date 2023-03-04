de8c005734fe2a7a5b7ea7173367a7e2f2b9a02f
lxhcaicai <2778763221@qq.com>
2023-03-04 12:16:00 +0800

    Calendar 类

--- src/main/java/org/example/date/DoTest.java
+++ src/main/java/org/example/date/DoTest.java
@@ -1,12 +1,14 @@
 package org.example.date;
 
+import com.sun.org.apache.bcel.internal.generic.PUSH;
 import org.junit.Test;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DoTest {
     /**
-     * Date类
+     * Date类 表示当前的日期对象，精确到毫秒值。
      */
     @Test
     public void test() {
@@ -20,5 +22,43 @@ public class DoTest {
 
         date.setTime(1632638970000L);
         System.out.println("date = " + date);
+    }
+
+    /**
+     * Calendar 类
+     */
+    @Test
+    public void test2() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        int milliSecond = calendar.get(Calendar.MILLISECOND);
+        System.out.print(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second + ":" + milliSecond);
+    }
+
+    @Test
+    public void test3() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2022);
+        System.out.println("calendar = " + calendar.get(Calendar.YEAR));
+    }
+
+    @Test
+    public void test4() {
+        Calendar calendar = Calendar.getInstance();
+        // 日历向后，偏移180天
+        calendar.add(Calendar.DAY_OF_MONTH, 180);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH) + 1;
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        int second = calendar.get(Calendar.SECOND);
+        int milliSecond = calendar.get(Calendar.MILLISECOND);
+        System.out.print(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second + ":" + milliSecond);
     }
 }
