1af6347b0d4902f54b08aedb8a61d56ebaaea99f
lxhcaicai <2778763221@qq.com>
2023-03-06 22:17:00 +0800

    获取修饰符

--- src/main/java/org/example/classload/demo2/DoTest.java
+++ src/main/java/org/example/classload/demo2/DoTest.java
@@ -2,6 +2,8 @@ package org.example.classload.demo2;
 
 import org.junit.Test;
 
+import java.lang.reflect.Modifier;
+
 public class DoTest {
     public static void main(String[] args) throws ClassNotFoundException {
         Class<?> clazz = Person.class;
@@ -26,5 +28,15 @@ public class DoTest {
         Package aPackage = clazz.getPackage();
         String name = aPackage.getName();
         System.out.println("name = " + name);
+    }
+
+    // 获取修饰符（修饰符定义在 Modifier 类中，该类里面有很多常量值，每一个常量对应一种修饰符）
+    @Test
+    public void test1() {
+        Class<?> clazz = Person.class;
+
+        int modifiers = clazz.getModifiers();
+        String s = Modifier.toString(modifiers);
+        System.out.println("s = " + s);
     }
 }
