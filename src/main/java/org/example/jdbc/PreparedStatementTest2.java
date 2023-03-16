8b04166874be2df177f525dc488cba627a8c69c6
lxhcaicai <2778763221@qq.com>
2023-03-17 00:34:16 +0800

    操作BLOB字段

---
+++ src/main/java/org/example/jdbc/PreparedStatementTest2.java
@@ -0,0 +1,62 @@
+package org.example.jdbc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PreparedStatementTest2 {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+
+    @Before
+    public void Before() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3307/test?useSSL=false&useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Shanghai";
+            String user = "root";
+            String password = "root";
+            connection = DriverManager.getConnection(url,user, password);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void insert() throws SQLException, FileNotFoundException {
+        String sql = "INSERT INTO `user`(username, password, photo) VALUES (?,?,?)";
+        preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, "lisi");
+        preparedStatement.setString(2, "123456");
+        preparedStatement.setBlob(3, new FileInputStream("C:\\Users\\he\\Pictures\\发布课程信息.png"));
+        int executeUpdate = preparedStatement.executeUpdate();
+        System.out.println("executeUpdate = " + executeUpdate);
+    }
+
+    @After
+    public void after() {
+        if(null != preparedStatement) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(null != connection) {
+            try{
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
